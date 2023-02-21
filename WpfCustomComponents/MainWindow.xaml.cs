d26fae6ac2597733b090ae5ccb89638dc87094d5
bburling <brett.burling@arcadis.com>
2023-02-22 10:14:44 +1100

    Initial commit

---
+++ WpfCustomComponents/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomComponents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //add some items to the combo
+            var items = new List<string>();
+            items.Add("Item01");
+            items.Add("Item02");
+            items.Add("Item03");
+            items.Add("Item04");
+            items.Add("Item04");
+
+
+            combo.ItemsSource =items;
+        }
+
+        private void pbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Primary Button was clicked");
+        }
+
+        private void sbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Secondary Button was clicked");
+        }
+    }
+}
